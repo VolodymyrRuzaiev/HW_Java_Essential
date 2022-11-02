6db3c9ff479332c13079ce1201233836a9243d77
VolodymyrRuzaiev <vladimir.ruzaev@gmail.com>
2022-11-02 20:55:40 +0200

    Less_002

--- _002_Class_and_Object/src/com/task_005_MyArea/Main.java
+++ _002_Class_and_Object/src/com/task_005_MyArea/Main.java
@@ -1,4 +1,22 @@
 package com.task_005_MyArea;
+/**
+ * Создать класс MyArea, в нем объявить константу PI = 3.14
+ * и статический метод areaOfCircle,
+ * который должен принимать радиус и используя PI
+ * посчитать площадь круга.
+ * Создать класс Main, где запустить данный метод.
+ *
+ */
 
 public class Main {
+    public static void main(String[] args) {
+
+
+        MyArea area_1 = new MyArea(10);
+
+        System.out.println("Area = " + area_1.area0fCircle());
+
+
+
+    }
 }
